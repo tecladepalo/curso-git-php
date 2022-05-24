653c8af3cf07bca60badba5460b2dc2942c0023f
tecladepalo <tecladepalo@gmail.com>
2022-05-24 08:03:34 -0400

    Added img folder and images styles

--- index.php
+++ index.php
@@ -26,9 +26,11 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
             Proin nonummy, lacus eget pulvinar lacinia, pede felis dignissim leo, vitae tristique magna lacus sit amet eros. Nullam ornare. Praesent odio ligula, dapibus sed, tincidunt eget, dictum ac, nibh. Nam quis lacus. Nunc eleifend molestie velit. Morbi lobortis quam eu velit. Donec euismod vestibulum massa. Donec non lectus. Aliquam commodo lacus sit amet nulla. Cras dignissim elit et augue. Nullam non diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In hac habitasse platea dictumst. Aenean vestibulum. Sed lobortis elit quis lectus. Nunc sed lacus at augue bibendum dapibus.
         </p>
         
+        <img src="img/chomi-42.JPG" width="300"></br>
+        
         
   <?php 
-        echo "Esta es una línea PHP";
+        echo "Chomi, cuando no era tierra todavía";
    ?>
         
         
